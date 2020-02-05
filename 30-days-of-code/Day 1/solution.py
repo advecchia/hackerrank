041d76f2da8f78ebee767ae9492b97712f23b48d
advecchia <advecchia@inf.ufrgs.br>
2020-02-05 01:33:33 -0300

    Adding solution for data types challenge.

---
+++ 30-days-of-code/Day 1/solution.py
@@ -0,0 +1,20 @@
+import sys
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+input_int = int(input())
+input_double = float(input())
+input_string = sys.stdin.readline().strip()
+
+# Print the sum of both integer variables on a new line.
+print(i + input_int)
+
+# Print the sum of the double variables on a new line.
+print("{:.1f}".format(d + input_double))
+
+# Concatenate and print the String variables on a new line
+print(s + input_string)
+# The 's' variable above should be printed first.
