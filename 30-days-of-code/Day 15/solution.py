97b79b1ab36ec9ea38f38a11a3f53dfb627e9c48
advecchia <advecchia@inf.ufrgs.br>
2020-02-24 13:16:32 -0300

    Day 15, adding solution for linked list.

--- 30-days-of-code/Day 15/solution.py
+++ 30-days-of-code/Day 15/solution.py
@@ -1,21 +1,35 @@
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
+
+
 class Solution:
-    def display(self,head):
+    def display(self, head):
         current = head
         while current:
-            print(current.data,end=' ')
+            print(current.data, end=' ')
             current = current.next
 
-    def insert(self,head,data):
-    #Complete this method
+    def insert(self, head, data):
+        new_node = Node(data)
 
-mylist= Solution()
-T=int(input())
-head=None
+        if head is None:
+            head = new_node
+            return head
+
+        last = head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+        return head
+
+
+mylist = Solution()
+T = int(input())
+head = None
 for i in range(T):
-    data=int(input())
-    head=mylist.insert(head,data)
-mylist.display(head);
+    data = int(input())
+    head = mylist.insert(head, data)
+mylist.display(head)
