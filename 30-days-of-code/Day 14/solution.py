5e6d099b59fe7de1fb21b4253f5df6dcb57064dd
advecchia <advecchia@inf.ufrgs.br>
2020-02-24 12:12:21 -0300

    Day 14, solution for absolute difference in limited scope.

--- 30-days-of-code/Day 14/solution.py
+++ 30-days-of-code/Day 14/solution.py
@@ -1,10 +1,37 @@
+MIN_INPUT_LENGTH = 1
+MAX_INPUT_LENGTH = 10
+MIN_ELEMENT_INTEGER = 1
+MAX_ELEMENT_INTEGER = 100
+
+
 class Difference:
+
     def __init__(self, a):
         self.__elements = a
 
-	# Add your code here
+    def validateElementValue(self, number):
+        if MIN_ELEMENT_INTEGER > number or number > MAX_ELEMENT_INTEGER:
+            raise ValueError('Invalid element value: ' + str(number))
 
-# End of Difference class
+    def computeDifference(self):
+        length = len(self.__elements)
+        if MIN_INPUT_LENGTH > length or length > MAX_INPUT_LENGTH:
+            raise ValueError('Invalid elements list length.')
+
+        temp_list = list(self.__elements)
+        self.maximumDifference = -9999999
+        for i in range(0, length - 1):
+            a = self.__elements[i]
+            self.validateElementValue(a)
+
+            temp_list.pop(0)
+            for b in temp_list:
+                self.validateElementValue(b)
+                difference = abs(a - b)
+
+                if self.maximumDifference < difference:
+                    self.maximumDifference = difference
+
 
 _ = input()
 a = [int(e) for e in input().split(' ')]
