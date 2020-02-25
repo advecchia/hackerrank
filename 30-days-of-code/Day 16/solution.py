011a4ffdfee2265c354b58d7a864a36296b5c772
advecchia <advecchia@inf.ufrgs.br>
2020-02-25 14:41:25 -0300

    Day 16, solving exception handling problem.

--- 30-days-of-code/Day 16/solution.py
+++ 30-days-of-code/Day 16/solution.py
@@ -1,6 +1,25 @@
 #!/bin/python3
 
-import sys
+"""
+MIN_INPUT_LENGTH = 1
+MAX_INPUT_LENGTH = 6
+
+# Hacker didn't allow conditional validation for this problem, so constraints are not validated.
+# Need to remove below commented code to problem pass on compiler.
+def validate_input_length(input_string):
+    if MIN_INPUT_LENGTH > len(input_string) or len(input_string) > MAX_INPUT_LENGTH:
+        raise ValueError('Invalid input string length.')
+"""
+
+
+def print_input_string(input_string):
+    try:
+        print(int(input_string))
+
+    except ValueError:
+        print('Bad String')
 
 
 S = input().strip()
+# validate_input_length(S)
+print_input_string(S)
