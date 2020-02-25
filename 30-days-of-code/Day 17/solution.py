bc7002ad5e81996467e16da181a2bef92f62c94e
advecchia <advecchia@inf.ufrgs.br>
2020-02-25 15:09:53 -0300

    Day 17 adding solution for power calculator.

--- 30-days-of-code/Day 17/solution.py
+++ 30-days-of-code/Day 17/solution.py
@@ -1,11 +1,27 @@
-#Write your code here
+import math
 
-myCalculator=Calculator()
-T=int(input())
+NON_NEGATIVE_ERROR_MESSAGE = 'n and p should be non-negative'
+
+
+class Calculator:
+    def __init__(self):
+        pass
+
+    def power(self, num, pw):
+        if num < 0 or pw < 0:
+            raise Exception(NON_NEGATIVE_ERROR_MESSAGE)
+
+        return int(math.pow(num, pw))
+
+
+myCalculator = Calculator()
+T = int(input())
 for i in range(T):
-    n,p = map(int, input().split())
+    n, p = map(int, input().split())
+
     try:
-        ans=myCalculator.power(n,p)
+        ans = myCalculator.power(n, p)
         print(ans)
+
     except Exception as e:
-        print(e)   
+        print(e)
