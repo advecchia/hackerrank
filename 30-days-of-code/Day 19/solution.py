73b47b903804beda4e84f8513bb31b0226c37aa2
advecchia <advecchia@inf.ufrgs.br>
2020-02-26 23:37:54 -0300

    Day 19 solution for sum of divisors problem.

--- 30-days-of-code/Day 19/solution.py
+++ 30-days-of-code/Day 19/solution.py
@@ -1,11 +1,37 @@
+from math import ceil
+from functools import reduce
+
+
 class AdvancedArithmetic(object):
-    def divisorSum(n):
+    def divisorSum(self, n):
         raise NotImplementedError
 
 
 class Calculator(AdvancedArithmetic):
+    __MIN_INPUT_VALUE = 1
+    __MAX_INPUT_VALUE = 1000
+
     def divisorSum(self, n):
-        pass
+        self.__validateInputValue(n)
+        divisors = self.__findDivisors(n)
+        return reduce(lambda x, y: x + y, divisors)
+
+    def __findDivisors(self, inputValue):
+        divisors = [1]
+        if inputValue == 1:
+            return divisors
+
+        limit = ceil(inputValue/2)
+        for i in range(2, limit + 1):
+            if inputValue % i == 0:
+                divisors.append(i)
+        divisors.append(inputValue)
+
+        return divisors
+
+    def __validateInputValue(self, inputValue):
+        if self.__MIN_INPUT_VALUE > inputValue or inputValue > self.__MAX_INPUT_VALUE:
+            raise ValueError('Invalid input value.')
 
 
 n = int(input())
