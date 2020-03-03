c79dfb65d2ef96fed84c9d034a5c464296389ef0
advecchia <advecchia@inf.ufrgs.br>
2020-03-03 01:40:27 -0300

    DAy 24, linked list problem init.

---
+++ 30-days-of-code/Day 24/solution.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Solution:
+    def insert(self, head, data):
+            p = Node(data)
+            if head is None:
+                head = p
+            elif head.next is None:
+                head.next=p
+            else:
+                start = head
+                while start.next is not None:
+                    start = start.next
+                start.next=p
+
+            return head
+
+    def display(self, head):
+        current = head
+        while current:
+            print(current.data, end=' ')
+            current = current.next
+
+    def removeDuplicates(self, head):
+        # Write your code here
+        pass
+
+
+mylist = Solution()
+T = int(input())
+head = None
+for i in range(T):
+    data = int(input())
+    head = mylist.insert(head, data)
+
+head = mylist.removeDuplicates(head)
+mylist.display(head)
