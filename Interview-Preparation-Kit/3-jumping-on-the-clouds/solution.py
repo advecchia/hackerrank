f5a411088398cfa588e1b1bbae230e5a57301569
advecchia <advecchia@inf.ufrgs.br>
2020-03-25 21:38:41 -0300

    Jumping clouds solution finished.

--- Interview-Preparation-Kit/3-jumping-on-the-clouds/solution.py
+++ Interview-Preparation-Kit/3-jumping-on-the-clouds/solution.py
@@ -1,23 +1,56 @@
 #!/bin/python3
 
-import math
 import os
-import random
-import re
-import sys
+
+MIN_NUMBER_OF_CLOUDS = 2
+MAX_NUMBER_OF_CLOUDS = 100
+VALID_SAFE_CLOUD = 0
+
+
+def validate_cloud_number(cloud_number):
+    if MIN_NUMBER_OF_CLOUDS > cloud_number or  cloud_number > MAX_NUMBER_OF_CLOUDS:
+        raise ValueError('Invalid cloud number.')
+
+
+def validate_cloud_inputs(cloud_number, cloud_array_length):
+    if not cloud_number == cloud_array_length:
+        raise ValueError('Invalid cloud array length.')
+
+
+def validate_game_start(cloud):
+    if not cloud == VALID_SAFE_CLOUD:
+        raise ValueError('Invalid start value for game.')
+
 
 # Complete the jumpingOnClouds function below.
 def jumpingOnClouds(c):
+    jumps = 0
+    index = 0
+    cloud_array_length = len(c) - 1
+
+    validate_game_start(c[index])
+
+    while True:
+        if index + 2 <= cloud_array_length and c[index + 2] == VALID_SAFE_CLOUD:
+            index += 2
+        elif index + 1 <= cloud_array_length and c[index + 1] == VALID_SAFE_CLOUD:
+            index += 1
+        else:
+            break
+
+        jumps += 1
+
+    return jumps
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input())
+    validate_cloud_number(n)
 
     c = list(map(int, input().rstrip().split()))
+    validate_cloud_inputs(n, len(c))
 
     result = jumpingOnClouds(c)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
